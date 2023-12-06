eccdc995a8a40c66ff9a3e72fe63913d0156b193
Jamessysystem <smenardjames@gmail.com>
2023-12-06 21:21:38 +0800

    4th Commit (Button toast)

--- app/src/main/java/com/sayson/menardjames/block6/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/sayson/menardjames/block6/p1/quiz/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sayson.menardjames.block6.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button1)
+
+        button.setOnClickListener(){
+            Toast.makeText(this,"Button ito is clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }

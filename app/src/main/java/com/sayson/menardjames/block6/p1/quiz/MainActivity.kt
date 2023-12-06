b77825f3ae05475ec648767fc87021eacbd00b1a
Jamessysystem <smenardjames@gmail.com>
2023-12-06 21:39:14 +0800

    5th Commit (Button to next Activity)

--- app/src/main/java/com/sayson/menardjames/block6/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/sayson/menardjames/block6/p1/quiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sayson.menardjames.block6.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,14 +9,20 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     private lateinit var button : Button
+    private lateinit var button2 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
 
         button.setOnClickListener(){
             Toast.makeText(this,"Button ito is clicked", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener(){
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
